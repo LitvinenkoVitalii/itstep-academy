1fa90e0c581bc949f88294b16d29cde8c8d739b1
LitvinenkoVitalii <reskator95@gmail.com>
2021-11-10 01:10:49 +0200

    Complex

---
+++ CS HW3 (Litvinenko)/CS HW3 (Litvinenko)/Complex.cs
@@ -0,0 +1,80 @@
+﻿namespace CS_HW3__Litvinenko_
+{
+    class Complex
+	{
+		double _real;
+		double _imaginary;
+
+		public double Real
+		{
+			get { return _real; }
+			set { _real = value; }
+		}
+
+		public double Iimaginary
+		{
+			get { return _imaginary; }
+			set { _imaginary = value; }
+		}
+
+
+		public Complex()
+		{
+			_real = 0;
+			_imaginary = 0;
+		}
+
+		public Complex(double real_part, double imaginary_part)
+		{
+			_real = real_part;
+			_imaginary = imaginary_part;
+		}
+
+
+		public override string ToString()
+		{
+			if (_imaginary == 0)
+			{
+				return $"{_real}";
+			}
+			else
+            {
+				return $"{_real} + {_imaginary}i ";
+			}
+		}
+
+		public static Complex operator +(Complex number1, Complex number2)
+        {
+			Complex res = new Complex();
+			res._real = (number1._real + number2._real);
+			res._imaginary = (number1._imaginary + number2._imaginary);
+
+			return res;
+		}
+		public static Complex operator -(Complex number1, Complex number2)
+        {
+			Complex res = new Complex();
+			res._real = (number1._real - number2._real);
+			res._imaginary = (number1._imaginary - number2._imaginary);
+
+			return res;
+		}
+		public static Complex operator *(Complex number1, Complex number2)                  //z = z1 * z2 = (a1a2 - b1b2) + (a1b2 + b1a2)i
+		{
+			Complex res = new Complex();
+			res._real = ((number1._real * number2._real) - (number1._imaginary * number2._imaginary));
+			res._imaginary = ((number1._real * number2._imaginary) + (number1._imaginary * number2._real));
+
+			return res;
+		}
+		public static Complex operator /(Complex number1, Complex number2)                  //z = z1 / z2 = ((a1a2 + b1b2) / a2^2 + b2^2) + ((a2b1 - a1b2) / a2^2 + b2^2)i
+		{
+			Complex res = new Complex();
+			res._real = (((number1._real * number2._real) + (number1._imaginary * number2._imaginary)) / ((number2._real * number2._real) + (number2._imaginary * number2._imaginary)));
+			res._imaginary = (((number2._real * number1._imaginary) - (number1._real * number2._imaginary)) / ((number2._real * number2._real) + (number2._imaginary * number2._imaginary)));
+
+			return res;
+		}
+
+}
+}
---
+++ CS HW3 (Litvinenko)/CS HW3 (Litvinenko)/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_HW3__Litvinenko_
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			Console.WriteLine("Введите действительную и мнимую часть комплексного числа №1: ");
+			double a1 = Convert.ToDouble(Console.ReadLine());
+			double b1 = Convert.ToDouble(Console.ReadLine());
+
+			Complex number1 = new Complex(a1, b1);
+
+			Console.WriteLine("Введите действительную и мнимую часть комплексного числа №2: ");
+			double a2 = Convert.ToDouble(Console.ReadLine());
+			double b2 = Convert.ToDouble(Console.ReadLine());
+
+			Complex number2 = new Complex(a2, b2);
+
+			Console.WriteLine("Комплексное число №1: {0}\nКомплексное число №1: {1}", number1, number2);
+
+			Console.WriteLine( " Что Вы хотите сделать с этими числами?");
+
+			char symbol = 'X';
+
+			do
+			{
+				Console.WriteLine();
+				Console.WriteLine(" Прибавить: '+'");
+				Console.WriteLine(" Вычесть: '-'");
+				Console.WriteLine(" Умножить: '*'");
+				Console.WriteLine(" Разделить: '/'");
+				Console.WriteLine(" Выход из программы - 0\n\n");
+
+				symbol = Convert.ToChar(Console.ReadLine());
+
+				switch (symbol)
+				{
+
+					case '+':
+
+						Console.WriteLine(number1 + number2);
+
+						break;
+					case '-':
+
+						Console.WriteLine(number1 - number2);
+
+						break;
+					case '*':
+
+						Console.WriteLine(number1 * number2);
+
+						break;
+					case '/':
+
+						Console.WriteLine(number1 / number2);
+
+						break;
+
+					default:
+						if (symbol != '0')
+						{
+							Console.WriteLine("Некорректный ввод! \n");
+						}
+
+						break;
+				}
+			} while (symbol != '0');
+
+		}
+	}
+}
